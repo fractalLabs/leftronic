e203150f8a5ccfccddc2ed94a029ccdc6a2493e5
patz <jorge@fractalmedia.mx>
2012-12-13 14:48:14 -0600

    expesion para separar por tabs corregida, doall para forzar la ejecucion de la lista, parseo de la salida cd sh-c y -main con arg de el numero de seg a esperar PD sleep usa segundos, no milisegundos

--- src/leftronic/cronr.clj
+++ src/leftronic/cronr.clj
@@ -1,15 +1,18 @@
 (ns leftronic.cronr
-  (:use [leftronic.core json]
+  (:use [leftronic.core ]
         [cj shell]))
 
 
 
-(defn lee [] (map #(re-seq #"[^\t+]" %) (re-seq #"[^\n]+" (slurp "crons.txt"))))
+(defn lee [] (map #(re-seq #"[^\t]+" %) (re-seq #"[^\n]+" (slurp "crons.txt"))))
 
-(defn ejecuta [l] (map #(point (first %) (sh-c (second %))) l))
+(defn ejecuta [l] (doall (map #(point (first %) (re-find #"\d+" (sh-c (second %)))) l)))
 
-(defn -main []
+(defn -main
+  ([seconds]
    (let [l (lee)]
-      (while true
-         (do (ejecuta l)
-             (sleep 60000)))))
+         (while true
+           (do (ejecuta l)
+             (sleep seconds)))))
+  ([]
+   (-main 60)))
